fc8e458ff7006909968f84967e6b85dc44a16c37
Geca13 <karafiloski84@gmail.com>
2021-07-01 15:06:50 +0200

    small update

---
+++ src/components/ProfileImageWithDefault.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import defaultPicture from '../assets/profile.png'
+
+const ProfileImageWithDefault = (props) => {
+
+    let imageSource = defaultPicture;
+    if(props.image){
+        imageSource=`/images/profile/${props.image}`
+    }
+    return (
+       
+            <img {...props} src={imageSource}  onError={
+                event => {
+                    event.target.src = defaultPicture;
+                }
+            }/>
+            
+    );
+};
+
+export default ProfileImageWithDefault;
---
+++ src/components/ProfileImageWithDefault.spec.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import ProfileImageWithDefault from './ProfileImageWithDefault'
+
+describe('ProfileImageWithDefault', ()=>{
+    describe('Layout', ()=>{
+        it('has image', ()=>{
+            const { container } = render(<ProfileImageWithDefault/>);
+            const image = container.querySelector('img');
+            expect(image).toBeInTheDocument();
+        });
+        it('displays default image when image property is not provided', ()=>{
+            const { container } = render(<ProfileImageWithDefault/>);
+            const image = container.querySelector('img');
+            expect(image.src).toContain('/profile.png');
+        });
+        it('displays user image when image property is provided', ()=>{
+            const { container } = render(<ProfileImageWithDefault image='profile1.png'/>);
+            const image = container.querySelector('img');
+            expect(image.src).toContain('/images/profile/profile1.png');
+        });
+        it('displays default image when provided image loading fails', ()=>{
+            const { container } = render(<ProfileImageWithDefault image='profile1.png'/>);
+            const image = container.querySelector('img');
+            fireEvent.error(image);
+            expect(image.src).toContain('/profile.png');
+        });
+    });
+});
