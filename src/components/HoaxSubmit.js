3aeb23f615a6ed35f683a142beea4b8914217e4e
Mishel <you@example.comkarafiloski84@gmail.comgit config --global user.name github_usernameGeca13>
2021-07-06 23:43:12 +0200

    working on hoaxes

--- src/components/HoaxSubmit.js
+++ src/components/HoaxSubmit.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react'
 import ProfileImageWithDefault from './ProfileImageWithDefault'
 import { connect } from 'react-redux'
 
-export default class HoaxSubmit extends Component {
+ class HoaxSubmit extends Component {
+
+    state = {
+        focused: false
+    }
+
+    onFocus = () =>{
+        this.setState({
+            focused: true
+        })
+    }
+
+    onClickCancel = () =>{
+        this.setState({
+            focused: false
+        })
+    }
+   
     render() {
         return (
             <div className='card d-flex flex-row p-1'>
@@ -11,7 +28,11 @@ export default class HoaxSubmit extends Component {
                 width='32' height='32'
                 image={this.props.loggedInUser.image} />
                 <div className='flex-fill'>
-                <textarea className='form-control w-100' rows={1}/>
+                <textarea onFocus={this.onFocus} className='form-control w-100' rows={this.state.focused ? 3 :1}/>
+                {this.state.focused && (<div className='text-right mt-1'>
+                <button className= 'btn btn-success'>Hoaxify</button>
+                <button onClick={this.onClickCancel} className= 'btn btn-light ml-1'><i className='fas fa-times'></i> Cancel</button>
+                </div>)}
                 </div>
             </div>
         )
--- src/components/HoaxSubmit.spec.js
+++ src/components/HoaxSubmit.spec.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import HoaxSubmit from './HoaxSubmit'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import authReducer from '../redux/authReducer';
+import * as apiCalls from '../api/apiCalls'
 
 
 const defaultState = {
@@ -51,5 +52,50 @@ describe('HoaxSubmit' , () =>{
             expect(image.src).toContain('/images/profile/' + defaultState.image);
           })
     })
+
+    describe('Interactions' , () =>{
+      it('display three rows when focused to textarea' , () =>{
+        const { container } = setup();
+        const textarea = container.querySelector('textarea');
+        fireEvent.focus(textarea)
+        expect(textarea.rows).toBe(3);
+      });
+      it('displays hoaxify button when focused to textarea' , () =>{
+        const { container, queryByText } = setup();
+        const textarea = container.querySelector('textarea');
+        fireEvent.focus(textarea);
+        const hoaxifyButton = queryByText('Hoaxify')
+        expect(hoaxifyButton).toBeInTheDocument();
+      });
+      it('displays cancel button when focused to textarea' , () =>{
+        const { container, queryByText } = setup();
+        const textarea = container.querySelector('textarea');
+        fireEvent.focus(textarea);
+        const cancelButton = queryByText('Cancel')
+        expect(cancelButton).toBeInTheDocument();
+      });
+      it('does not display hoaxify button when textarea is not focused' , () =>{
+        const { queryByText } = setup();
+        
+        const hoaxifyButton = queryByText('Hoaxify')
+        expect(hoaxifyButton).not.toBeInTheDocument();
+      });
+
+      it('does not display hoaxify button when textarea is not focused' , () =>{
+        const { queryByText } = setup();
+        
+        const cancelButton = queryByText('Cancel')
+        expect(cancelButton).not.toBeInTheDocument();
+      });
+      it('returns back to unfocused after clicking the cancel button' , () =>{
+        const { container, queryByText } = setup();
+        const textarea = container.querySelector('textarea');
+        fireEvent.focus(textarea);
+        const cancelButton = queryByText('Cancel')
+        fireEvent.click(cancelButton);
+        expect(queryByText('Cancel')).not.toBeInTheDocument();
+      });
+      
+  })
 
 })
